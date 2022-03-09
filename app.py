2fe81b65f01099afa07fe689ab335985f5e0f052
JINKA RANGA BHARATH <bharathjinka09@gmail.com>
2022-03-09 12:50:19 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import os, time, sys
+
+folder_path = "C:\Files_To_Read"
+file_ends_with = ".txt"
+how_many_days_old_logs_to_remove = 7
+
+now = time.time()
+only_files = []
+
+for file in os.listdir(folder_path):
+    file_full_path = os.path.join(folder_path,file)
+    if os.path.isfile(file_full_path) and file.endswith(file_ends_with):
+        #Delete files older than x days
+        if os.stat(file_full_path).st_mtime < now - how_many_days_old_logs_to_remove * 86400: 
+             os.remove(file_full_path)
+             print "\n File Removed : " , file_full_path
